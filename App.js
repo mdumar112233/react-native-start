4e0fa3db42cf4d11baed835e402247f969524154
md umar faruk <mdumar11233>
2021-04-20 11:53:35 +0600

    react native start

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState(null);
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUser(data))
+  },[])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>hello working on your app!</Text>
+      <Text>{name}</Text>
+      <Image 
+        source={{
+          uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+        }}
+        style={{width: 200, height: 200}}
+      />
+      <TextInput
+      style={{
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1
+      }}
+      onChangeText={text => setName(text)}
+      />
       <StatusBar style="auto" />
     </View>
   );
